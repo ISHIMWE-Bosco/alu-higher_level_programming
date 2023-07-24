5702bd4e612947d1c4d64c1daa65f2a68fd3c6ee
ISHIMWE-Bosco <b.ishimwe3@alustudent.com>
2023-07-24 15:38:59 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 
+no module imported
+"""
+
+class square: 
+    """
+    Defines a square by private instance size,
+    instantiation with size.
+    """
+
+    def _init_(self, size):
+        """
+        size : no type/value verification
+        ----------------------------
+        Private instance attribute size
+        """
+        self.__size = size
+
